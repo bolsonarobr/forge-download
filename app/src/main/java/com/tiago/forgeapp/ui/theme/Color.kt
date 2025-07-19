27aa1b036c1747f6a1ed1fb29a2789dfab938134
Tiago <tiagoridao@gmail.com>
2025-07-19 16:45:59 -0300

    botoes criado

--- app/src/main/java/com/tiago/forgeapp/ui/theme/Color.kt
+++ app/src/main/java/com/tiago/forgeapp/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.tiago.forgeapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BlueButton = Color(0xFF2196F3)
+val GreenButton = Color(0xFF4CAF50)
+val RedButton = Color(0xFFD32F2F)
+val DarkGreyPanel = Color(0xFF121212)
+val BlackBackground = Color(0xFF000000)
--- app/src/main/java/com/tiago/forgeapp/ui/theme/Theme.kt
+++ app/src/main/java/com/tiago/forgeapp/ui/theme/Theme.kt
@@ -10,34 +10,39 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = BlueButton,
+    secondary = GreenButton,
+    tertiary = RedButton,
+    background = BlackBackground,
+    surface = DarkGreyPanel,
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onBackground = Color.White,
+    onSurface = Color.White,
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = BlueButton,
+    secondary = GreenButton,
+    tertiary = RedButton,
+    background = BlackBackground,
+    surface = DarkGreyPanel,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
 )
 
 @Composable
 fun ForgeappTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
